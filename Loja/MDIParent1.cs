dd2057b58d13c81344550cbb57e0dab999df25c7
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2020-04-24 23:43:08 -0300

    Multiple Document Interface (MDI)

--- Loja/MDIParent1.cs
+++ Loja/MDIParent1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Loja
 {
-    public partial class Form1 : Form
+    public partial class MDIParent1 : Form
     {
-        public Form1()
+        public MDIParent1()
         {
             InitializeComponent();
         }
